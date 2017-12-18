8d807bcc0033e78b7f743d1a02e8ade5dc8087ea
zhouhuafei <zhouhuafei@shopex.cn>
2017-12-18 14:46:09 +0800

    vue

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import app from './app.vue';
 // 公共的样式
 require('./scss/commons/common.scss');
 
-setTimeout(function () {
-    new Vue({
-        el: '#app',
-        template: '<app></app>',
-        components: {app},
-    });
-}, 0);
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        new Vue({
+            el: '#app',
+            template: '<app></app>',
+            components: {app},
+        });
+    }, 0);
+});

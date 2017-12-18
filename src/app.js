1257ec0e2daf7b745aa701859fd98bac69b95b53
zhouhuafei <zhouhuafei@shopex.cn>
2017-12-18 14:57:21 +0800

    setimeout

--- src/app.js
+++ src/app.js
@@ -4,10 +4,8 @@ import app from './app.vue';
 // 公共的样式
 require('./scss/commons/common.scss');
 
-setTimeout(function () {
-    new Vue({
-        el: '#app',
-        template: '<app></app>',
-        components: {app},
-    });
-}, 0);
+new Vue({
+    el: '#app',
+    template: '<app></app>',
+    components: {app},
+});
